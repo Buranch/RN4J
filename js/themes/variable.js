47cdbfc5e042eb134a80e61fd2e78182f6fe9549
Shivraj <shivraj@geekyants.com>
2017-07-04 20:27:24 +0530

    upgrade

--- js/themes/variable.js
+++ js/themes/variable.js
@@ -1,20 +1,19 @@
-
-const color = require('color');
+const color = require("color");
 
 // theme background
-const primary = color('#01cca1');
+const primary = color("#01cca1");
 // header
-const secondary = color('#00c497');
-const info = color('#5bc0de');
-const success = color('#5cb85c');
-const danger = color('#d9534f');
-const warning = color('#f0ad4e');
-const sidebar = color('#252932');
-const dark = color('rgba(0,0,0,0.8)');
-const light = color('rgba(255,255,255,0.8)');
+const secondary = color("#00c497");
+const info = color("#5bc0de");
+const success = color("#5cb85c");
+const danger = color("#d9534f");
+const warning = color("#f0ad4e");
+const sidebar = color("#252932");
+const dark = color("rgba(0,0,0,0.8)");
+const light = color("rgba(255,255,255,0.8)");
 
-const theme1 = '#01cca1';		// theme background
-const header1 = '#00c497';	// header
+const theme1 = "#01cca1"; // theme background
+const header1 = "#00c497"; // header
 
 // var theme2 = "#E74C3C";
 // var header2 ="#CB4335";
@@ -29,14 +28,14 @@ const header1 = '#00c497';	// header
 // var header5 = "#34495E";
 
 module.exports = {
-  brandPrimary: primary.hexString().toString(),
-  brandSecondary: secondary.hexString().toString(),
-  brandInfo: info.hexString().toString(),
-  brandSuccess: success.hexString().toString(),
-  brandDanger: danger.hexString().toString(),
-  brandWarning: warning.hexString().toString(),
-  brandSidebar: sidebar.hexString().toString(),
-  // darker: darken,
-  dark: dark.hexString().toString(),
-  light: light.hexString().toString(),
+	brandPrimary: primary.hex().toString(),
+	brandSecondary: secondary.hex().toString(),
+	brandInfo: info.hex().toString(),
+	brandSuccess: success.hex().toString(),
+	brandDanger: danger.hex().toString(),
+	brandWarning: warning.hex().toString(),
+	brandSidebar: sidebar.hex().toString(),
+	// darker: darken,
+	dark: dark.hex().toString(),
+	light: light.hex().toString(),
 };
