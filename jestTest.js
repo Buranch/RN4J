430ed850983574926633f58e4e2beb5fb52fee01
Shivraj <shivraj@geekyants.com>
2017-08-07 17:02:36 +0530

    jest script test2

--- jestTest.js
+++ jestTest.js
@@ -1,6 +1,6 @@
 var path = require("path");
 const exec = require("child_process").exec;
-exec("node " + path.join("node_modules", ".bin", "jest"), (error, stdout, stderr) => {
+exec("node " + path.join("node_modules", ".bin", "jest -i"), (error, stdout, stderr) => {
 	if (error || stdout) {
 		console.error(
 			"There are eslint errors in your project. Please run 'yarn run eslint [filename or dirname]' for more info."
