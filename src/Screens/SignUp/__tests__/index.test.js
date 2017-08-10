d8871d57594e6f794c4a2a7babda8d6371044d5f
Shivraj <shivraj@geekyants.com>
2017-08-10 18:49:00 +0530

    jest test fixed

--- src/Screens/SignUp/__tests__/index.test.js
+++ src/Screens/SignUp/__tests__/index.test.js
@@ -1,3 +1,15 @@
-it("works", () => {
-	expect(1).toBe(1);
+/**
+ * Sample React Native Snapshot Test
+ */
+"use strict";
+
+import "react-native";
+import React from "react";
+import SignUp from "../index";
+// Note: test renderer must be required after react-native.
+import renderer from "react-test-renderer";
+
+it("renders correctly", () => {
+	const tree = renderer.create(<SignUp />).toJSON();
+	expect(tree).toMatchSnapshot();
 });
